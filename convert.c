5fe6471d312794ba4cbe5bbed38d97049d4d9b30
Mashallah17 <32356001+Mashallah17@users.noreply.github.com>
2017-09-28 22:42:51 +0300

    Update convert.c

--- convert.c
+++ convert.c
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/* Функции для перевода из шкалы в шкалу*/
 float ctof (float x){
 	return x*1.8+32;
 }
@@ -18,46 +17,44 @@ float ktoc (float x){
 
 int main(int argc, char *argv[]){
 	float c = atof(argv[1]);
-	/* Если указана шкала*/
 	if (argc==3)
 		switch (*argv[2]){
 			case 'C':
 			case 'c':
 				if (ctok(c) < 0)
-					printf("Menshe absolutnogo nulya!\n");
+					printf("Temperature is less than absolute zero!\n");
 				else
 					printf("%.2lf F\n%.2f K", ctof(c), ctok(c));
 				break;
 			case 'F':
 			case 'f':
 				if (ctok(ftoc(c)) < 0)	
-					printf("Menshe absolutnogo nulya!\n");
+					printf("Temperature is less than absolute zero!\n");
 				else	
 					printf("%.2f C\n%.2f K", ftoc(c), ctok(ftoc(c)));
 				break;
 			case 'K':
 			case 'k':
 				if (c < 0)
-					printf("Menshe absolutnogo nulya!\n");
+					printf("Temperature is less than absolute zero!\n");
 				else
 					printf("%.2f C\n%.2f F", ktoc(c), ctof(ktoc(c)));
 				break;
 		}
-	/* Если не указана шкала*/
 	else {
 		printf ("%.2f C:\n", c);
 		if (ctok(c) < 0)
-			printf("Menshe absolutnogo nulya!\n");
+			printf("Temperature is less than absolute zero!\n");
 		else
 			printf("%.2f F\n%.2f K\n", ctof(c), ctok(c));
 		printf ("%.2f F:\n", c);
 		if (ctok(ftoc(c)) < 0)	
-			printf("Menshe absolutnogo nulya!\n");
+			printf("Temperature is less than absolute zero!\n");
 		else
 			printf("%.2f C\n%.2f K\n", ftoc(c), ctok(ftoc(c)));
 		printf ("%.2f K:\n", c);
 		if (c < 0)
-			printf("Menshe absolutnogo nulya!\n");
+			printf("Temperature is less than absolute zero!\n");
 		else
 			printf("%.2f C\n%.2f F\n", ktoc(c), ctof(ktoc(c)));
 		}
